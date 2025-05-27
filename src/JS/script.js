08330c3167298411296fa96c7c22e24f1c0b082a
Tomé <tome.rossi@gmail.com>
2025-05-27 10:03:28 -0300

    Adicionando estrutura basica da landing page

---
+++ src/JS/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded',()=>{
+//DECLARANDO AS VARIAVEIS usando DOM
+const loginForm=document.getElementById('form-login');
+const mensagem=document.getElementById('message');
+const video=document.querySelector('.video-container');
+const loginContainer=document.getElementById('login-container');
+const buttonEmtrar=document.getElementById('btnEntrar');
+const buttonSair=document.getElementById('btnSair');
+
+//VERIFICA SE O USUARIO ESTA LOGADO NO LOCALSTORAGE
+
+
+
+})
