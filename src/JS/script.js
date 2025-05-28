263e8ba7c367e32d23b5453cc51c3f30144d6969
Tomé <tome.rossi@gmail.com>
2025-05-28 11:42:16 -0300

    Adicionando Pagina de Login

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,14 +1,8 @@
-document.addEventListener('DOMContentLoaded',()=>{
-//DECLARANDO AS VARIAVEIS usando DOM
-const loginForm=document.getElementById('form-login');
-const mensagem=document.getElementById('message');
-const video=document.querySelector('.video-container');
-const loginContainer=document.getElementById('login-container');
-const buttonEmtrar=document.getElementById('btnEntrar');
-const buttonSair=document.getElementById('btnSair');
-
-//VERIFICA SE O USUARIO ESTA LOGADO NO LOCALSTORAGE
-
-
-
-})
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('form-login');
+    const mensagem = document.getElementById('message');
+    const video = document.querySelector('.video-container');
+    const loginContainer = document.getElementById('login-container');
+    const buttonEntrar = document.getElementById('btnEntrar');
+    const buttonSair = document.getElementById('btnSair');
+}
