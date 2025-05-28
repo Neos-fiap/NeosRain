177fdc66ee0ee1162898493d1d2e1c6432bf4532
Carlos Goes <goescadu5@gmail.com>
2025-05-28 11:28:41 -0300

    comentando todo o codigo para ficar mais facil o entendimento

--- src/JS/mobile-navbar.js
+++ src/JS/mobile-navbar.js
@@ -1,13 +1,17 @@
+// Classe responsável por controlar o menu mobile (hamburguer)
 class MobileNavbar {
   constructor(mobileMenu, navList, navLinks) {
+    // Seleciona o botão do menu, a lista de navegação e os links
     this.mobileMenu = document.querySelector(mobileMenu);
     this.navList = document.querySelector(navList);
     this.navLinks = document.querySelectorAll(navLinks);
     this.activeClass = "active";
 
+    // Garante que o 'this' funcione corretamente no evento de clique
     this.handleClick = this.handleClick.bind(this);
   }
 
+  // Animação dos links do menu ao abrir/fechar
   animateLinks() {
     this.navLinks.forEach((link, index) => {
       link.style.animation
@@ -18,16 +22,19 @@ class MobileNavbar {
     });
   }
 
+  // Função chamada ao clicar no menu hamburguer
   handleClick() {
     this.navList.classList.toggle(this.activeClass);
     this.mobileMenu.classList.toggle(this.activeClass);
     this.animateLinks();
   }
 
+  // Adiciona o evento de clique ao menu hamburguer
   addClickEvent() {
     this.mobileMenu.addEventListener("click", this.handleClick);
   }
 
+  // Inicializa o menu mobile
   init() {
     if (this.mobileMenu) {
       this.addClickEvent();
@@ -36,6 +43,7 @@ class MobileNavbar {
   }
 }
 
+// Instancia a classe passando os seletores do menu
 const mobileNavbar = new MobileNavbar(
   ".mobile-menu",
   ".nav-list",
